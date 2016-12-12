e20dd1d8cdbdb5001f587fca62587ffbe34de64d
Bastien <bastien@MacBook-Bastien.local>
2016-12-13 00:33:51 +0100

    v.1.21
    Structure index et slider provisoire

--- index.php
+++ index.php
@@ -1,8 +1,51 @@
 <!-- Bastien Nicoud - Think Analog -->
 <!-- Projet semestriel - novembre 2016 -->
 
+<!-- appel du header avec la navigation -->
 <?php require "includes/headernav.php"; ?>
 
+<div class="container">
 
+  <section id="slider">
+    <div class="row">
 
+      <!-- Slider -->
+      <div id="slider-page-acceuil" class="carousel slide second-plan" data-ride="carousel">
+        <div class="carousel-inner" role="listbox">
+          <div class="item active">
+            <img src="img/slider/1.png" alt="lightning" class="width-maximum">
+          </div>
+          <div class="item">
+            <img src="img/slider/2.png" alt="wp" class="width-maximum">
+          </div>
+          <div class="item">
+            <img src="img/slider/3.png" alt="ma2" class="width-maximum">
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </section>
+
+  <section id="welcome">
+    <div class="row">
+      
+    </div>
+  </section>
+
+  <section id="skills">
+    <div class="row">
+      
+    </div>
+  </section>
+
+  <section id="team">
+    <div class="row">
+      
+    </div>
+  </section>
+
+</div>
+
+<!-- appel du footer -->
 <?php require "includes/footer.php"; ?>
