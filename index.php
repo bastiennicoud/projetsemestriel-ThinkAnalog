64f3624f8ea4efd53ccd8a2f7f2fe49a94554b26
Bastien <bastien@MacBook-Bastien.local>
2016-12-13 21:59:35 +0100

    v1.22
    Slider terminé avec images officielles rendu 3D.
    Travail sur la page d’accueil.

--- index.php
+++ index.php
@@ -4,8 +4,6 @@
 <!-- appel du header avec la navigation -->
 <?php require "includes/headernav.php"; ?>
 
-<div class="container">
-
   <section id="slider">
     <div class="row">
 
@@ -13,13 +11,13 @@
       <div id="slider-page-acceuil" class="carousel slide second-plan" data-ride="carousel">
         <div class="carousel-inner" role="listbox">
           <div class="item active">
-            <img src="img/slider/1.png" alt="lightning" class="width-maximum">
+            <img src="img/slider/ok1.jpg" alt="lightning" class="width-maximum">
           </div>
           <div class="item">
-            <img src="img/slider/2.png" alt="wp" class="width-maximum">
+            <img src="img/slider/ok4.jpg" alt="wp" class="width-maximum">
           </div>
           <div class="item">
-            <img src="img/slider/3.png" alt="ma2" class="width-maximum">
+            <img src="img/slider/ok6.jpg" alt="ma2" class="width-maximum">
           </div>
         </div>
       </div>
@@ -27,10 +25,74 @@
     </div>
   </section>
 
+<div class="container">
+
   <section id="welcome">
+
     <div class="row">
-      
+      <div class="col-sm-8 col-sm-offset-2">
+        <div class="media">
+          <div class="media-left">
+              <img class="media-object" src="..." alt="logo">
+          </div>
+          <div class="media-body">
+            <h4 class="media-heading">Bienvenue</h4>
+            <p>
+              Bienvenue sur Think Analog
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
+
+    <div class="row">
+      <div class="col-sm-8 col-sm-offset-2">
+        <div class="media">
+          <div class="media-left">
+              <img class="media-object" src="..." alt="logo">
+          </div>
+          <div class="media-body">
+            <h4 class="media-heading">Nos produits</h4>
+            <p>
+              Bienvenue sur Think Analog
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+    <div class="row">
+      <div class="col-sm-8 col-sm-offset-2">
+        <div class="media">
+          <div class="media-left">
+              <img class="media-object" src="..." alt="logo">
+          </div>
+          <div class="media-body">
+            <h4 class="media-heading">Nos valeurs</h4>
+            <p>
+              Bienvenue sur Think Analog
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+    <div class="row">
+      <div class="col-sm-8 col-sm-offset-2">
+        <div class="media">
+          <div class="media-left">
+              <img class="media-object" src="..." alt="logo">
+          </div>
+          <div class="media-body">
+            <h4 class="media-heading">Qualité</h4>
+            <p>
+              Bienvenue sur Think Analog
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+
   </section>
 
   <section id="skills">
