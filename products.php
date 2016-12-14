c9a57131fe6ea15ab4b1dc056252200625dafb98
Bastien <bastien@MacBook-Bastien.local>
2016-12-14 12:00:56 +0100

    v1.26
    Affichage des produits sur la page produit.

--- products.php
+++ products.php
@@ -1,6 +1,37 @@
 <!-- Bastien Nicoud - Think Analog -->
 <!-- Projet semestriel - novembre 2016 -->
 
+<!-- Partie php pour récuperer les produits -->
+<?php
+
+  // connexion a la bd
+  require_once "includes/connectbd.php";
+
+  // requete pour recuperer tous les produits
+  // Preparation de la requète
+  if (!$req = $dbconn->prepare("
+                                SELECT products.name, products.header, images.title, images.src, categorys.category
+                                FROM products
+                                INNER JOIN images ON products.id_product=images.idx_product
+                                INNER JOIN categorys ON products.id_product=categorys.idx_product
+                                ")) {
+    // Gestion des erreurs
+    $errors['preparation'] = "Erreur de preparation de la requete";
+  }
+      
+  // execution de la requete
+  if (!$req->execute()) {
+    // Gestion des erreurs
+    $errors['execution'] = "Erreur d'execution de la requete";
+  }
+
+  // recuper le resultat et conversion en tableau
+  $res = $req->get_result();
+  $row = $res->fetch_all();
+
+?>
+
+
 <!-- appel du header avec la navigation -->
 <?php require "includes/headernav.php"; ?>
 
@@ -8,18 +39,18 @@
 <section id="products">
 
   <div class="container">
-    <div class="row">
+    <div class="row section-first">
 
       <!-- *************************************** -->
       <!-- Partie pour la selection par categories -->
       <!-- *************************************** -->
-      <div class="col-sm-4">
+      <div class="col-sm-3">
 
         <div class="thumbnail">
           <div class="caption">
-            <h3>Filtres</h3>
+            <h4>Filtres</h4>
             <p>Filtrez les produits affichés par catégorie</p>
-            
+
             <div class="radio">
               <label>
                 <input type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked>
@@ -62,7 +93,23 @@
       <!-- *************************************** -->
       <!-- Affichage des produits                  -->
       <!-- *************************************** -->
-      <div class="col-sm-8">
+      <div id="products-display" class="col-sm-9">
+
+        <?php foreach ($row as $key): ?>
+
+          <div class="thumbnail">
+            <div class="row">
+              <div class="col-sm-4">
+                <img src="<?= $key[3] ?>" title="<?= $key[2] ?>" width="100%">
+              </div>
+              <div class="col-sm-8">
+                <h3><?= $key[0] ?> <small><?= $key[4] ?></small></h3>
+                <p><?= $key[1] ?></p>
+              </div>
+            </div>
+          </div>
+
+        <?php endforeach; ?>
         
       </div>
 
